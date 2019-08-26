58d57c84894d0e962a0320fdf0d6c1aa0cf326b1
mark a. foltz <mfoltz@chromium.org>
2019-08-26 15:29:47 -0700

    Adds locales and agent-info-event

--- scripts/openscreen_cddl_dfns.py
+++ scripts/openscreen_cddl_dfns.py
@@ -3,6 +3,10 @@
 # specification.  Only these types will be wrapped in <dfn> tags and treated as
 # definitions.
 LINKED_TYPES = frozenset([
+  'agent-info',
+  'agent-info-event',
+  'agent-info-request',
+  'agent-info-response',
   'presentation-change-event',
   'presentation-connection-close-event',
   'presentation-connection-message',
